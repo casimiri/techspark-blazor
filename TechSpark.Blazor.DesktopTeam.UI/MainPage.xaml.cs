1d716aefc6eb9dc0ed2c4182eaf368c4ca31a92e
Casimir, COMPAORE <casiseba@hotmail.com>
2022-04-05 04:20:00 +0200

    Desktop & mobile

---
+++ TechSpark.Blazor.DesktopTeam.UI/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace TechSpark.Blazor.DesktopTeam.UI
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ TechSpark.Blazor.DesktopTeam.UI/Platforms/iOS/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace TechSpark.Blazor.DesktopTeam.UI
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
